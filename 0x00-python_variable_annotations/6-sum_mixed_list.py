8bba6b5f7873bfe23a6aecf4fd64c4b0fbcae775
0sei531 <141256948+0sei531@users.noreply.github.com>
2024-07-04 02:41:41 -1200

    Update 6-sum_mixed_list.py

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -3,7 +3,8 @@
 Description: Takes a list mxd_lst of floats and integers
     and returns their sum as a float.
 Arguments:
-    mxd_lst (List[Union[int, float]]): A list of integers and float numbers to sum up.
+    mxd_lst (List[Union[int, float]]): A list of integers and
+    float numbers to sum up.
 """
 
 
@@ -15,7 +16,8 @@ def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     Return the sum of elements in mxd_lst.
 
     Args:
-        mxd_lst (List[Union[int, float]]): A list of integers and float numbers.
+        mxd_lst (List[Union[int, float]]): A list of integers
+        and float numbers.
 
     Returns:
         float: The sum of all numbers in the mxd_lst.
