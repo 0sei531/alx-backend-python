2fc6339af6358dd25d3ec597f434a75aa37c0dcc
0sei531 <141256948+0sei531@users.noreply.github.com>
2024-07-04 01:55:25 -1200

    Create 0-add.py

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Description: Type-annotated function add that takes a float a and a float b
+             as arguments and returns their sum as a float.
+"""
+
+def add(a: float, b: float) -> float:
+    """
+    Return the sum of a and b.
+
+    Args:
+        a (float): The first float number.
+        b (float): The second float number.
+
+    Returns:
+        float: The sum of the two float numbers.
+    """
+    return a + b
