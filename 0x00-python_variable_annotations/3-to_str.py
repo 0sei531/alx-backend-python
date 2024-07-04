e10d4de4a9fca9af1af22637a0602a874c5513f4
0sei531 <141256948+0sei531@users.noreply.github.com>
2024-07-04 02:21:23 -1200

    Create 3-to_str.py

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Module 3-to_str
+Defines a function to_str which takes a float and returns its string representation.
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Return the string representation of the float n.
+
+    Args:
+        n (float): The float number to convert to a string.
+
+    Returns:
+        str: The string representation of the float number.
+    """
+    return str(n)
