c1e7982ecd853d5f378bd97de8c41719dda2475f
0sei531 <141256948+0sei531@users.noreply.github.com>
2024-07-04 02:11:42 -1200

    Create 2-floor.py

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+Description: A module that defines a function floor which takes a float n
+             as an argument and returns the floor of the float.
+"""
+
+
+import math
+
+def floor(n: float) -> int:
+    """
+    Return the largest int value less than or equal to n.
+
+    Args:
+        n (float): The float number to floor.
+
+    Returns:
+        int: The floor value of the float number.
+    """
+    return math.floor(n)
