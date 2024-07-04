1a7e18bcc11f07b8dd66b95f3e334bb45e52b9a8
0sei531 <141256948+0sei531@users.noreply.github.com>
2024-07-04 02:02:01 -1200

    Create 1-concat.py

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+Description: A module that defines a function concat which takes 
+        two strings and returns their concatenation.
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Concatenate two strings.
+
+    Args:
+        str1 (str): The first string.
+        str2 (str): The second string.
+
+    Returns:
+        str: The concatenated string.
+    """
+    return str1 + str2
