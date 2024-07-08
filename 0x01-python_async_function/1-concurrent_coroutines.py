084b1140e1ca757041ef00a4b39e1ea3a7952ad9
0sei531 <141256948+0sei531@users.noreply.github.com>
2024-07-08 09:17:15 -1200

    Update 1-concurrent_coroutines.py

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -15,6 +15,7 @@ from typing import List
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
+
 async def wait_n(n: int, max_delay: int = 10) -> List[float]:
     """
     Waits for a random delay until max_delay, returns
