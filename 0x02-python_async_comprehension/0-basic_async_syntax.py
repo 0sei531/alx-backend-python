33d2e8127be22ef319a606b9265644da55dd095c
0sei531 <141256948+0sei531@users.noreply.github.com>
2024-07-09 09:21:14 -1200

    Update 0-basic_async_syntax.py

--- 0x02-python_async_comprehension/0-basic_async_syntax.py
+++ 0x02-python_async_comprehension/0-basic_async_syntax.py
@@ -1,15 +1,15 @@
- #!/usr/bin/env python3
+#!/usr/bin/env python3
 """
 This module contains a coroutine that generates random numbers asynchronously.
 """
 
-from asyncio import sleep
-from random import uniform
+import asyncio
+import random
 from typing import AsyncGenerator
 
 
 async def async_generator() -> AsyncGenerator[float, None]:
-    """Generator that yields a random value between 0 and 10 every second, 10 times."""
+    """Asynchronously generates random numbers between 0 and 10."""
     for _ in range(10):
-        await sleep(1)
-        yield uniform(0, 10)
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
