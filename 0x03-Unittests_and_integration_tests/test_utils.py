50518f770e59e8aa93ef76cecfda25483b3b3518
0sei531 <141256948+0sei531@users.noreply.github.com>
2024-07-29 02:40:42 -1200

    Update test_utils.py

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -15,7 +15,9 @@ class TestAccessNestedMap(unittest.TestCase):
         ({"a": {"b": 2}}, ("a",), {'b': 2}),
         ({"a": {"b": 2}}, ("a", "b"), 2)
     ])
-    def test_access_nested_map(self, nested_map: Dict, path: Tuple[str, ...], expected: Union[Dict, int]):
+    def test_access_nested_map(self, nested_map: Dict,
+                               path: Tuple[str, ...],
+                               expected: Union[Dict, int]):
         """Test that access_nested_map returns the expected result"""
         self.assertEqual(access_nested_map(nested_map, path), expected)
 
@@ -23,7 +25,9 @@ class TestAccessNestedMap(unittest.TestCase):
         ({}, ("a",), KeyError),
         ({"a": 1}, ("a", "b"), KeyError)
     ])
-    def test_access_nested_map_exception(self, nested_map: Dict, path: Tuple[str, ...], exception: Exception):
+    def test_access_nested_map_exception(self, nested_map: Dict,
+                                         path: Tuple[str, ...],
+                                         exception: Exception):
         """Test that access_nested_map raises the correct exception"""
         with self.assertRaises(exception):
             access_nested_map(nested_map, path)
